4f14e470aabfce4f324ad2eb6fe58878c65c45d9
Romiko Derbynew <romikov@gmail.com>
2019-12-27 20:19:34 +1100

    Added TrolleyTotal and also ensure floats are used for total, not int.

---
+++ WooliesX.Api/Controllers/TrolleyTotalController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WooliesX.Infrastructure.ApiClient;
+
+namespace WooliesX.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TrolleyTotalController : ControllerBase
+    {
+        private readonly ILogger<TrolleyTotalController> _logger;
+        private readonly IConfiguration _config;
+        private Client _client;
+
+        public TrolleyTotalController(ILogger<TrolleyTotalController> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+            _client = new Client(new HttpClient());
+            _client.BaseUrl = _config.GetValue<string>("WooliesXBaseUrl");
+        }
+
+        [HttpPost]
+        public async Task<float> Post(Trolley trolley)
+        {
+            return await _client.ApiResourceTrolleyCalculatorPostAsync(_config.GetValue<Guid>("ApiToken"), trolley);
+        }
+    }
+}
