3a8d07674df1623e2af88833af9a3a677e69f16a
Romiko Derbynew <romikov@gmail.com>
2019-12-27 18:12:35 +1100

    Added APIClient for WooliesX external resource.

--- WooliesX.Api/Controllers/SortController.cs
+++ WooliesX.Api/Controllers/SortController.cs
@@ -1,5 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WooliesX.Infrastructure.ApiClient;
 
 namespace WooliesX.Api.Controllers
 {
@@ -9,17 +15,22 @@ namespace WooliesX.Api.Controllers
     {
 
         private readonly ILogger<SortController> _logger;
+        private readonly IConfiguration _config;
+        private Client _client;
 
-        public SortController(ILogger<SortController> logger)
+        public SortController(ILogger<SortController> logger, IConfiguration config)
         {
             _logger = logger;
+            _config = config;
+            _client = new Client(new HttpClient());
+            _client.BaseUrl = _config.GetValue<string>("WooliesXBaseUrl");
         }
 
         [HttpGet]
-        public object Get()
+        public async Task<ICollection<Product>> Get()
         {
-            //ToDo Excercise 2
-            return null;
+            var products = await _client.ApiResourceProductsGetAsync(_config.GetValue<Guid>("ApiToken"));
+            return products;
         }
     }
 }
