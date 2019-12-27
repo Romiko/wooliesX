499db428a53154bd730cb12f25d918ad553bd9a4
Romiko Derbynew <romikov@gmail.com>
2019-12-27 19:19:02 +1100

    Added Recommended option for sort. Only get popular products. Assumption - Average Price

--- WooliesX.Api/Controllers/SortController.cs
+++ WooliesX.Api/Controllers/SortController.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using WooliesX.Infrastructure.ApiClient;
@@ -13,6 +14,7 @@ namespace WooliesX.Api.Controllers
     [Route("[controller]")]
     public class SortController : ControllerBase
     {
+        public enum SortOptions { Low, High, Ascending, Descending, Recommended }
 
         private readonly ILogger<SortController> _logger;
         private readonly IConfiguration _config;
@@ -27,10 +29,36 @@ namespace WooliesX.Api.Controllers
         }
 
         [HttpGet]
-        public async Task<ICollection<Product>> Get()
+        public async Task<ICollection<Product>> Get(SortOptions sortOption)
         {
-            var products = await _client.ApiResourceProductsGetAsync(_config.GetValue<Guid>("ApiToken"));
-            return products;
+            if (sortOption != SortOptions.Recommended)
+            {
+                var products = await _client.ApiResourceProductsGetAsync(_config.GetValue<Guid>("ApiToken"));
+
+                switch (sortOption)
+                {
+                    case SortOptions.Low:
+                        return products.OrderBy(p => p.Price).ToList();
+                    case SortOptions.High:
+                        return products.OrderByDescending(p => p.Price).ToList();
+                    case SortOptions.Ascending:
+                        return products.OrderBy(p => p.Name, StringComparer.OrdinalIgnoreCase).ToList();
+                    case SortOptions.Descending:
+                        return products.OrderByDescending(p => p.Name, StringComparer.OrdinalIgnoreCase).ToList();
+                    default:
+                        return products;
+                }
+            }
+
+            var shoppingOrders = await _client.ApiResourceShopperHistoryGetAsync(_config.GetValue<Guid>("ApiToken"));
+
+            return (
+                from p in shoppingOrders.SelectMany(o => o.Products)
+                group p by p.Name into pgroup
+                select new Product { Name = pgroup.Key, Price = pgroup.Average(p => p.Price), Quantity = pgroup.Sum(a => a.Quantity) }
+                )
+                .OrderByDescending(x => x.Quantity)
+                .ToList();
         }
     }
 }
