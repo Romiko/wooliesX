7af06b91d4b821505453e833852dcad800d11495
Romiko Derbynew <romikov@gmail.com>
2019-12-27 20:36:42 +1100

    Clean up usings

--- WooliesX.Api/Controllers/TrolleyTotalController.cs
+++ WooliesX.Api/Controllers/TrolleyTotalController.cs
@@ -2,8 +2,6 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using WooliesX.Infrastructure.ApiClient;
